00f4a1f4c1d8e329e6ed0f16b4dd462a5a47b23c
dimonluk <lds4ever2000@gmail.com>
2017-10-12 21:00:52 +0300

    Everythong except login is rewritten in angular

--- project-app/src/app/app.module.ts
+++ project-app/src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppComponent } from "./components/app/app.component";
 import { MainPageComponent } from './components/mainPage/mainPage.component';
 import { LecturesComponent } from "./components/lectures/lectures.component";
 import { HomeworkComponent } from "./components/homework/homework.component";
+import { InfoComponent } from "./components/info/info.component";
+import { MeetingsComponent } from "./components/meetings/meetings.component";
 import { RouterModule, Routes } from "@angular/router";
 const appRoutes: Routes = [
 	{path:"", component:MainPageComponent},
 	{path:"lectures", component:LecturesComponent},
-	{path:"homework", component:HomeworkComponent}
+	{path:"homework", component:HomeworkComponent},
+	{path:"info", component:InfoComponent},
+	{path:"meetings", component:MeetingsComponent}
 ];
 @NgModule({
   declarations: [
 	AppComponent,
     	MainPageComponent,
 	LecturesComponent,
-	HomeworkComponent
+	HomeworkComponent,
+	InfoComponent,
+	MeetingsComponent
   ],
   imports: [
 	BrowserModule,
