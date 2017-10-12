05e86fa438924b5f272688d6b97f4cb7482f6097
dimonluk <lds4ever2000@gmail.com>
2017-10-12 14:24:44 +0300

    Made some angular

--- project-app/src/app/app.module.ts
+++ project-app/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { MainPageComponent } from './components/mainPage/mainPage.component';
-
+import { LecturesComponent } from "./components/lectures/lectures.component";
+import { RouterModule, Routes } from "@angular/router";
+const appRoutes: Routes = [
+	{path:"", component:MainPageComponent},
+	{path:"lectures", component:LecturesComponent}
+];
 @NgModule({
   declarations: [
-    MainPageComponent
+    	MainPageComponent,
+	LecturesComponent
   ],
   imports: [
-    BrowserModule
+	BrowserModule,
+	RouterModule.forRoot(
+	appRoutes,
+	{enableTracing: true}
+)
   ],
   providers: [],
   bootstrap: [MainPageComponent]
---
+++ project-app/src/app/components/lectures/lectures.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+ 	selector:"lectures",
+	templateUrl: './lectures.component.html',
+	styleUrls: ['./lectures.component.css']
+})
+export class LecturesComponent {
+}
