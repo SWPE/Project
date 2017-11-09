2ce50e98e90cc2e90cfcfa2261cfe8a6c96a9f90
dimonluk <lds4ever2000@gmail.com>
2017-11-09 16:23:36 +0200

    Litle improvements in server

--- project-app/server/server.py
+++ project-app/server/server.py
@@ -3,38 +3,50 @@ import flask
 import MySQLdb
 import json
 import hashlib
-connection=MySQLdb.connect(user="projectAdmin", password="opiproject", host="127.0.0.1", database="project", charset="utf8")
-cursor = connection.cursor()
 app = Flask(__name__)
+def getCursor():
+    connection = MySQLdb.connectconnection=MySQLdb.connect(user="projectAdmin", password="opiproject", host="127.0.0.1", database="project", charset="utf8")
+    cursor = connection.cursor()
+    return (cursor, connection)
+def closers(cursor, connection):
+    cursor.close()
+    connection.close()
 @app.route("/getPeople")
 def getPeople():
+    cursor, connection = getCursor()
     cursor.execute("SELECT * FROM peopleTable;")
     response = []
     for id, name, number, isComing in cursor:
         response.append({"id":id,"name":name,"number":number, "isComing":isComing})
+    closers(cursor, connection)
     resp = flask.Response(json.dumps(response))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 @app.route("/getLectures")
 def getLectures():
+    cursor, connection = getCursor()
     cursor.execute("SELECT * FROM lecturesTable;")
     result = []
     for subject,filename,source,description in cursor:
         result.append({"subject":subject, "source":source, "fileName":filename,"description":description})
+    closers(cursor, connection)
     resp = flask.Response(json.dumps(result))
     resp.headers['Access-Control-Allow-Origin'] = "*"
     return resp
 @app.route("/getHomeworks")
 def getHomeworks():
+    cursor, c = getCursor()
     cursor.execute("SELECT * FROM homeworks;")
     result = []
     for subject, whenGiven, whenPass, textDescription, fileName, source in cursor:
         result.append({"subject":subject, "whenGiven":whenGiven, "whenPass":whenPass, "textDescription":textDescription, "fileName":fileName, "source":source});
+    closers(cursor, c)
     resp = flask.Response(json.dumps(result))
     resp.headers['Access-Control-Allow-Origin'] = "*"
     return resp
 @app.route("/getInfo")
 def getInfo():
+    cursor, c = getCursor()
     result = []
     cursor.execute("SELECT * FROM info;")
     for name, text, hash, date in cursor:
@@ -44,15 +56,18 @@ def getInfo():
         cursor.execute(executable)
         for name, source, hash in cursor:
             res["files"].append({"name":name, "source":source})
+    closers(cursor, c)
     resp = flask.Response(json.dumps(result))
     resp.headers['Access-Control-Allow-Origin'] = "*"
     return resp
 @app.route("/getMeeting")
 def getMeeting():
+    cursor, c = getCursor()
     result = {}
     cursor.execute("SELECT * FROM meeting;")
     for place, date, description in cursor:
         result = {"place":place, "description":description, "date":date}
+    closers(cursor, c)
     resp = flask.Response(json.dumps(result))
     resp.headers['Access-Control-Allow-Origin'] = "*"
     return resp
